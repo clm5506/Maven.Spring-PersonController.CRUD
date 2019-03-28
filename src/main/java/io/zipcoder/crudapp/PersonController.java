ede4d314b5d0c71eb03cbe76fac1a4144b0e0028
Cristina McClintock <cristinamcclintock@zipcoders-MacBook-Pro.local>
2019-03-28 15:54:34 -0400

    controller updates

--- src/main/java/io/zipcoder/crudapp/PersonController.java
+++ src/main/java/io/zipcoder/crudapp/PersonController.java
@@ -1,37 +1,41 @@
 package io.zipcoder.crudapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
 public class PersonController {
 //    @Autowired
-//    private PersonService service;
+//    private PersonService personService;
 //
 
     @GetMapping("/people/{id}")
     public ResponseEntity<Person> getPerson(@PathVariable int id){
-        return null;
+        return new ResponseEntity<Person>(personService.findOne(id), HttpStatus.OK);
     }
-
 
     @PostMapping("/people")
     public ResponseEntity<Person> createPerson(@PathVariable Person p){
-        return null;
+        return new ResponseEntity<Person>(personService.create(p), HttpStatus.CREATED);
     }
 
-    List<Person> getPersonList(){
-            return null;
+    @GetMapping("/people")
+    public List<Person> getPersonList(){
+        return new ResponseEntity<Person>(personService.findAll(), HttpStatus.OK);
     }
 
     @PutMapping("/people/{id}")
-    public ResponseEntity<Person> update(@PathVariable Person p){
-        
+    public ResponseEntity<Person> update(@PathVariable Person p, int id){
+        return new ResponseEntity<Person>(personService.update(id,p), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/people/{id}")
+    public ResponseEntity<Person> delete(@PathVariable int id){
+        return new ResponseEntity<Person>(personService.delete(id),HttpStatus.OK);
     }
 
 }
